7ac9c7d7eec098547b9216149e41bab68b3e2f39
Alaa <asyousse@us.ibm.com>
2015-06-02 13:28:31 -0400

    build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -v
+#export GOPATH=/c/src/containers-hijack-proxy
 export GOPATH=~/containers-hijack-proxy
-go build -o bin/hijack src/hijack.go
+go build -o bin/hijack src/hijack/hijack.go
 ls -l bin
