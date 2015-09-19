57eb5346ae30652eddcbb102c8b81311330295ed
Alaa <asyousse@us.ibm.com>
2015-09-19 09:41:44 -0400

    return ver with health response

--- src/handler/health.go
+++ src/handler/health.go
@@ -20,13 +20,17 @@ func HealthEndpointHandler(w http.ResponseWriter, r *http.Request) {
 	p := GetUriPattern(r.RequestURI, healthPatterns)
 	switch p{
 	case healthPatterns[0]:
-		log.Println("hjproxy up")
-		fmt.Fprintf(w,"hjproxy up\n")
+		v := conf.GetVerStr()
+		fmt.Fprintf(w,"hjproxy up\n%s\n", v)
+		log.Println("hjproxy up", v)
 		break
 	case healthPatterns[1]:
+		v := conf.GetVerStr()
 		n := conf.GetNumServedRequests()
-		log.Printf("This instance served %d requests\n", n)
+		fmt.Fprintf(w,"hjproxy %s\n", v)
 		fmt.Fprintf(w,"This instance served %d requests\n", n)
+		log.Println("hjproxy", v)
+		log.Printf("This instance served %d requests\n", n)
 		break
 	default:
 		log.Printf("Health pattern not accepted, URI=%s", r.RequestURI)
