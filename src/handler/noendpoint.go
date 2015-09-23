06c399edf5f7e4877b51ce438948e898e81e3cc9
Alaa <asyousse@us.ibm.com>
2015-09-22 22:45:35 -0400

    more verbose 401 and 404 errors

--- src/handler/noendpoint.go
+++ src/handler/noendpoint.go
@@ -3,11 +3,13 @@ package handler
 import (
 	"net/http"
 	"log"
+	"io"
 )
 
 //Return 404 for non-supported URIs
 func NoEndpointHandler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("NoEndpointHandler triggered, URI=%s, returning error 404\n", r.RequestURI)
-	//w.WriteHeader(404)
-	http.NotFound(w, r)
+	//http.NotFound(w, r)
+	w.WriteHeader(404)
+	io.WriteString(w, "404 not found (Hijackproxy NoEndpointHandler)\n")
 }
--- src/handler/notauth.go
+++ src/handler/notauth.go
@@ -10,5 +10,5 @@ import (
 func NotAuthorizedHandler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("NotAuthorizedHandler triggered, URI=%s, returning error 401\n", r.RequestURI)
 	w.WriteHeader(401)
-	io.WriteString(w, "401 not authorized\n")
+	io.WriteString(w, "401 not authorized (Hijackproxy NotAuthorizedHandler)\n")
 }
