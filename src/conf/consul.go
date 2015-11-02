a66f67f0aeb7c4190623ebf8b43e0ca048805abc
Alaa <asyousse@us.ibm.com>
2015-11-02 15:59:11 -0500

    More logging for Consul integration

--- src/conf/consul.go
+++ src/conf/consul.go
@@ -19,12 +19,16 @@ type ConsulService struct{
 func GetServiceHost(service string) (host string){
 	consul_host := GetConsulIp()+":"+strconv.Itoa(GetConsulPort())
 	consul_url := "http://"+consul_host+"/v1/catalog/service/"+service
-
+	Log.Printf("Will invoke Consul... url=%s", consul_url)
 	//invoke Consul to get service metadata
 	client := &http.Client{}
 	req, err := http.NewRequest("GET",consul_url, nil)
 	req.Header.Add("Accept", "application/json")
 	resp, err := client.Do(req)
+	if err != nil{
+		Log.Printf("Error connecting to Consul: %v", err)
+		return
+	}
 	defer resp.Body.Close()
 	body,_:=ioutil.ReadAll(resp.Body)
 	Log.Printf("Consul raw response: %s", string(body))
@@ -42,5 +46,6 @@ func GetServiceHost(service string) (host string){
 
 	//pick one of the endpoints
 	host = consulService[0].Address + ":" + strconv.Itoa(consulService[0].ServicePort)
+	Log.Printf("Consul response: host=%s", host)
 	return
 }
