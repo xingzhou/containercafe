ab201ab0e7079cf479b336cbe63a14b2c69265c7
Alaa <asyousse@us.ibm.com>
2015-11-23 22:06:00 -0500

    implement health handler using uri pattern router

--- src/handler/health.go
+++ src/handler/health.go
@@ -3,75 +3,76 @@ package handler
 import (
 	"net/http"
 	"fmt"
-	"strings"
 
 	"conf"
+	"auth"
 )
 
-// supported health api uri prefix patterns
-var healthPatterns = []string {
-	"/hjproxy/health",
-	"/hjproxy/stats",
-	"/hjproxy/_ping_notls", // _ping with no tls override regardless of hjproxy configuration
-	"/hjproxy/_ping",  		//  /hjproxy/_ping/host  or  /hjproxy/_ping/host/port  8089 is used if port not specified
-}
+var healthRouter *Router
 
 //called from init() of the package
-func InitHealthHandler(){}
+func InitHealthHandler(){
+	healthRoutes := []Route{
+		NewRoute("GET", "/hjproxy/health", healthHandler),
+		NewRoute("GET", "/hjproxy/stats", statsHandler),
+		NewRoute("GET", "/hjproxy/_ping/{node}", pingHandler),
+		NewRoute("GET", "/hjproxy/_ping/{node}/{port}", pingHandler),
+		NewRoute("GET", "/hjproxy/_ping_notls/{node}", pingNotlsHandler),
+		NewRoute("GET", "/hjproxy/_ping_notls/{node}/{port}", pingNotlsHandler),
+		NewRoute("*", "*", defaultHealthHandler),  //wildcard for everything else
+	}
+	healthRouter = NewRouter(healthRoutes)
+}
 
 func HealthEndpointHandler(w http.ResponseWriter, r *http.Request) {
 	Log.Printf("HealthEndpointHandler triggered, URI=%s", r.RequestURI)
-	p := GetUriPattern(r.RequestURI, healthPatterns)
-	switch p{
-	case healthPatterns[0]:
-		v := conf.GetVerStr()
-		fmt.Fprintf(w,"hjproxy up\n%s\n", v)
-		Log.Print("hjproxy up ", v)
-		break
-	case healthPatterns[1]:
-		v := conf.GetVerStr()
-		n := conf.GetNumServedRequests()
-		fmt.Fprintf(w,"hjproxy %s\n", v)
-		fmt.Fprintf(w,"This instance served %d non-admin requests\n", n)
-		Log.Print("hjproxy ", v)
-		Log.Printf("This instance served %d non-admin requests", n)
-		break
-	case healthPatterns[2]:
-		ping(w, r, true)
-		break
-	case healthPatterns[3]:
-		ping(w, r, false)
-		break
-	default:
-		Log.Printf("Health pattern not accepted, URI=%s", r.RequestURI)
-		NoEndpointHandler(w, r)
-	}
+	healthRouter.DoRoute(w, r, nil /*body*/, auth.Creds{}, "" /*req_id*/)
 }
 
-func ping(w http.ResponseWriter, r *http.Request, tls_override bool) {
-	var status int
+func healthHandler(w http.ResponseWriter, r *http.Request, body []byte, creds auth.Creds, vars map[string]string, req_id string){
+	v := conf.GetVerStr()
+	fmt.Fprintf(w,"hjproxy up\n%s\n", v)
+	Log.Print("hjproxy up ", v)
+}
 
-	sl := strings.Split(r.RequestURI, "/")
-	if len(sl)<=3 {
-		Log.Print("Error in _ping ... no host specified")
-		NoEndpointHandler(w, r)
-		return
-	}
-	server := sl[3]
+func statsHandler(w http.ResponseWriter, r *http.Request, body []byte, creds auth.Creds, vars map[string]string, req_id string){
+	v := conf.GetVerStr()
+	n := conf.GetNumServedRequests()
+	fmt.Fprintf(w,"hjproxy %s\n", v)
+	fmt.Fprintf(w,"This instance served %d non-admin requests\n", n)
+	Log.Print("hjproxy ", v)
+	Log.Printf("This instance served %d non-admin requests", n)
+}
+
+func pingHandler(w http.ResponseWriter, r *http.Request, body []byte, creds auth.Creds, vars map[string]string, req_id string) {
+	_ping(w, r, false, vars)
+}
+
+func pingNotlsHandler(w http.ResponseWriter, r *http.Request, body []byte, creds auth.Creds, vars map[string]string, req_id string) {
+	_ping(w, r, true, vars)
+}
+
+func defaultHealthHandler(w http.ResponseWriter, r *http.Request, body []byte, creds auth.Creds, vars map[string]string, req_id string){
+	Log.Printf("Health pattern not accepted, URI=%s", r.RequestURI)
+	NoEndpointHandler(w, r)
+}
+
+func _ping(w http.ResponseWriter, r *http.Request, tls_override bool, vars map[string]string){
+	//vars contains {node} and optional {port}
+	node := vars["{node}"]
 	port := conf.GetDockerPort()
-	if len(sl)>4 {
-		port = sl[4]
+	if _,ok := vars["{port}"]; ok {
+		port = vars["{port}"]
 	}
-	redirect_host := server + ":" + port
-
+	redirect_host := node + ":" + port
 	resp, err, _ := redirect (r, nil /*body*/, redirect_host, "" /*resource_id*/, pingRewriteUri, tls_override)
+	var status int
 	if (err != nil) {
 		Log.Printf("Error in redirection of _ping to %s ... err=%v", redirect_host, err)
 		status = 500
 	}else {
 		status = resp.StatusCode
 	}
-
 	if status == 200 {
 		Log.Printf("_ping success to host=%s", redirect_host)
 		fmt.Fprintf(w,"_ping success to host=%s\n", redirect_host)  //returns status 200
