e9a645d244b796c4ecde865704bdb9760e2dc85f
Alaa <asyousse@us.ibm.com>
2016-04-01 20:11:46 -0400

    update script to run as standalone docker on local machine

--- rundocker.sh
+++ rundocker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -v
 cd dockerize
-HOSTS=$(cat hosts)
-echo $HOSTS
-CMD="docker run $HOSTS hijack"
+#HOSTS=$(cat hosts)
+#echo $HOSTS
+CMD="docker run -v `pwd`:/opt/tls_certs -p 8087:8087 hijack"
 echo $CMD
 $(echo $CMD)
