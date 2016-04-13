f22df7a732ae462421411f8814f676b7f8b93861
Mariusz Sabath <sabath@us.ibm.com>
2016-04-13 11:19:15 -0400

    Modified the build script to be more generic.
    Also fixed the creds example

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -v
-#export GOPATH=/c/src/containers-hijack-proxy
-export GOPATH=~/containers-hijack-proxy
+PWD=`pwd`
+export GOPATH=$PWD:$GOPATH
+#export GOPATH=~/containers-hijack-proxy
 go build -o bin/hijack src/hijack/hijack.go
 ls -l bin
