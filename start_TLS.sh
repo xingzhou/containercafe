176cab64336eba39f7be4517cc22572060b61584
Alice Tarng <alicetarng@alices-mbp.watson.ibm.com>
2016-06-13 13:47:38 -0400

    Certificates moved to ~/.ice/certs/OpenRadiant/<API_key>

--- start_TLS.sh
+++ start_TLS.sh
@@ -57,7 +57,7 @@ if [ ! -f creds.json ]; then
 fi
 
 
-echo "{\"Status\":200, \"Node\":\"10.140.171.205:443\", \"Docker_id\":\"\", \"Container\": \"\", \"Swarm_shard\":true, \"Tls_override\":true, \"Space_id\":\"$SPACE_ID\", \"Reg_namespace\":\"swarm\", \"Apikey\":\"$API_KEY\", \"Orguuid\":\"orgname\", \"Userid\":\"userid\", \"Endpoint_type\":\"radiant\", \"TLS_path\":\"$PWD/user_certificates/$API_KEY\"}" >> creds.json
+echo "{\"Status\":200, \"Node\":\"10.140.171.205:443\", \"Docker_id\":\"\", \"Container\": \"\", \"Swarm_shard\":true, \"Tls_override\":true, \"Space_id\":\"$SPACE_ID\", \"Reg_namespace\":\"swarm\", \"Apikey\":\"$API_KEY\", \"Orguuid\":\"orgname\", \"Userid\":\"userid\", \"Endpoint_type\":\"radiant\", \"TLS_path\":\"~/.ice/certs/OpenRadiant/$API_KEY\"}" >> creds.json
 
 echo "Certificates created for Apikey "$API_KEY
-echo "Located at "$PWD"/user_certificates/"$API_KEY
+echo "Located at ~/.ice/certs/OpenRadiant/"$API_KEY
