394cd37e8a02a375d98985d9f97dc5747a1bdac6
Mariusz Sabath <sabath@us.ibm.com>
2016-07-13 18:44:12 -0400

    Added hjproxy container remove

--- proxy/rundocker.sh
+++ proxy/rundocker.sh
@@ -3,6 +3,9 @@ set -v
 cd dockerize
 #HOSTS=$(cat hosts)
 #echo $HOSTS
+CMD="docker rm -f hjproxy"
+echo $CMD
+$(echo $CMD)
 CMD="docker run -v `pwd`:/opt/tls_certs -p 8087:8087 --name hjproxy proxy"
 echo $CMD
 $(echo $CMD)
