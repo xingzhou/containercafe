394c416893f568a92fcaa75479ae9bbe9fe56774
Mariusz Sabath <sabath@us.ibm.com>
2016-07-13 13:03:46 -0400

    user certs from admin-certs

--- proxy/start_proxy.sh
+++ proxy/start_proxy.sh
@@ -1,8 +1,12 @@
 # override TLS_OUTBOUND to support K8s
 export tls_outbound=true
 export tls_inbound=true
-export server_key_file=certs/hjserver-key.pem 
-export server_cert_file=certs/hjserver.pem
+export server_key_file=admin-certs/hjserver.key 
+export server_cert_file=admin-certs/hjserver.pem
+export stub_auth_file="creds.json"
+# export kube_admin_key_file = "/opt/tls_certs/kadmin.key"
+# export kube_admin_cert_file = "/opt/tls_certs/kadmin.pem"
+
 # dev-mon01:
 export ccsapi_host=10.140.34.174:8081
 export consul_ip=10.140.34.174
