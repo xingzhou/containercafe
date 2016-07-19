8ee733908f95a7dac0bcb6f4ff87b985537fd9cf
Mariusz Sabath <sabath@us.ibm.com>
2016-07-19 16:26:59 -0400

    Modified proxy start with server certs

--- proxy/start_proxy.sh
+++ proxy/start_proxy.sh
@@ -1,8 +1,9 @@
 # override TLS_OUTBOUND to support K8s
 export tls_outbound=true
 export tls_inbound=true
-export server_key_file=certs/hjserver-key.pem 
-export server_cert_file=certs/hjserver.pem
+export server_key_file=admin-certs/hjserver.key 
+export server_cert_file=admin-certs/hjserver.pem
+export ca_cert_file=admin-certs/ca.pem
 # dev-mon01:
 export ccsapi_host=10.140.34.174:8081
 export consul_ip=10.140.34.174
