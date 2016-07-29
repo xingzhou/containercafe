859c9f9e4a60d1fbfff4b27590e030189a1f4b59
ALICE TARNG <atarng@us.ibm.com>
2016-07-29 12:29:59 -0400

    Cleaned lib folder of proxy-test-container

--- proxy/proxy-test-container/lib/setup_local.sh
+++
@@ -1,6 +0,0 @@
-export DOCKER_HOST=localhost:8087
-export DOCKER_TLS_VERIFY=0
-export DOCKER_CONFIG=fileauth
-title="Fileauth"
-echo -n -e "\033]0;$title\007"
-echo "Use:  DOCKER_TLS_VERIFY=\"\" docker ps"
