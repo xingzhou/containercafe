d59214f39a88a8017a24e5768115e2feb94d4eb4
ALICE TARNG <atarng@us.ibm.com>
2016-07-29 12:19:59 -0400

    Fixed tabs to spaces problem

--- proxy/proxy-test-container/lib/make_yaml.sh
+++ proxy/proxy-test-container/lib/make_yaml.sh
@@ -10,20 +10,20 @@ if [ ! -f $FILE_PATH ]; then
 	echo "apiVersion: v1" >> $FILE_PATH
 	echo "kind: Pod" >> $FILE_PATH
 	echo "metadata:" >> $FILE_PATH
-	echo "	name: $TENANT_ID-kube-web-test$NUM" >> $FILE_PATH
-	echo "	labels:" >> $FILE_PATH
-	echo "		app: web-ms-demo" >> $FILE_PATH
-	echo "	annotations:" >> $FILE_PATH
-	echo "		aaa: bbbb" >> $FILE_PATH
-	echo "		containers-label.alpha.kubernetes.io/com.swarm.tenant.0: sf7f413cb-a678-412d-b024-8e17e28bcb88-default" >> $FILE_PATH
+	echo "  name: $TENANT_ID-kube-web-test$NUM" >> $FILE_PATH
+	echo "  labels:" >> $FILE_PATH
+	echo "    app: web-ms-demo" >> $FILE_PATH
+	echo "  annotations:" >> $FILE_PATH
+	echo "    aaa: bbbb" >> $FILE_PATH
+	echo "    containers-label.alpha.kubernetes.io/com.swarm.tenant.0: sf7f413cb-a678-412d-b024-8e17e28bcb88-default" >> $FILE_PATH
 	echo "spec:" >> $FILE_PATH
-	echo "	containers:" >> $FILE_PATH
-	echo "		- name: kube-web-server" >> $FILE_PATH
-	echo "			image: mrsabath/web-ms:v3" >> $FILE_PATH
-	echo "			ports:" >> $FILE_PATH
-	echo "				- containerPort: 80" >> $FILE_PATH
-	echo "			env:" >> $FILE_PATH
-	echo "				-" >> $FILE_PATH
-	echo "				 name: \"TEST\"" >> $FILE_PATH
-	echo "				 value: \"web-test$NUM\"" >> $FILE_PATH
+	echo "  containers:" >> $FILE_PATH
+	echo "    - name: kube-web-server" >> $FILE_PATH
+	echo "      image: mrsabath/web-ms:v3" >> $FILE_PATH
+	echo "      ports:" >> $FILE_PATH
+	echo "        - containerPort: 80" >> $FILE_PATH
+	echo "      env:" >> $FILE_PATH
+	echo "        -" >> $FILE_PATH
+	echo "         name: \"TEST\"" >> $FILE_PATH
+	echo "         value: \"web-test$NUM\"" >> $FILE_PATH
 fi 
