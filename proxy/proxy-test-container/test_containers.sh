8dc5da3d62b84412a396c774da6c19e6087dfbf4
ALICE TARNG <atarng@us.ibm.com>
2016-07-29 12:42:02 -0400

    proxy-test-container is clean

--- proxy/proxy-test-container/test_containers.sh
+++ proxy/proxy-test-container/test_containers.sh
@@ -35,42 +35,7 @@ Note:
 HELPMEHELPME
 }
 
-# helpme()
-# {
-# 	cat <<HELPMEHELPME
 
-# Syntax: ${0} <proxy_location> <network_id> <tenant_id> <test_kube?> <num_containers> <num_pods> 
-# Where:
-# 	proxy_location =
-# 	    local - test is targeting local instance of proxy (localhost:8087) (default if argument not given)
-# 	    dev-mon01 - test is targeting remote instance (https://containers-api-dev.stage1.ng.bluemix.net:9443)
-
-# 	network_id = id of network element to be inspected (using default as default)
-
-# 	tenant_id = user_id of current user; should match Apikey being used in the config file. Currently, default is "test1".
-
-# 	test_kube? = "true" or "false". In case of multi-user testing, only 1 user should run the kube tests ("true"); all others should be "false". 
-
-# 	num_containers = total number of containers to be created; equal split between containers with net, and containers without. 
-# 		If argument not given, default value is 5. 
-
-# 	num_pods = total number of pods to be created. if argument not given, default value is 5. 
-
-	  
-
-# Note: 
-# 	when running local, specify whether autentication is done by CCSAPI:
-# 		export DOCKER_CONFIG=certs/ccsapi 
-# 	or local fileauth:
-# 		export DOCKER_CONFIG=certs/fileauth
-
-# 	*** If executing multiple-user fileauth, export DOCKER_CONFIG=certs/fileauth/<user_id> *** 
-
-# HELPMEHELPME
-# }
-
-
-# Help check
 if [[ "$1" == "" || "$1" == "-?" || "$1" == "-h" || "$1" == "--help" || "$1" == "help" ]] ; then
 	helpme
 	exit 1
@@ -118,23 +83,17 @@ if [[ "$TENANT_ID" == "" ]]; then
 fi 
 
 
-# NUM_NET_CONTAINERS=$((NUM_CONTAINERS / 2))
-# NUM_NO_NET_CONTAINERS=$((NUM_CONTAINERS - NUM_NET_CONTAINERS))
-
 NUM_NET_CONTAINERS=0 # No network for now
 NUM_NO_NET_CONTAINERS=$NUM_CONTAINERS
 
 
+source lib/common_test_func.sh 
 
-source lib/common_test_func.sh # I HOPE THIS WORKS
 
-
-# Setting the proxy location stuff
 case "$PROXY_LOC" in 
 	"local")
 		eval "export DOCKER_HOST=localhost:8087"
 		eval "export DOCKER_TLS_VERIFY=1"
-		#CMD_PREFIX="DOCKER_TLS_VERIFY=\"\""
 		CMD_PREFIX="\"\""
 		;;
 	"dev-mon01")
@@ -142,35 +101,9 @@ case "$PROXY_LOC" in
 		eval "export DOCKER_TLS_VERIFY=1"
 		eval "export DOCKER_CERT_PATH=certs/dev-mon01"
 		eval "export DOCKER_CONFIG=certs/dev-mon01"
-		# eval "title=\"dev-mon01 auth\""
-		# eval "echo -n -e "\033]0;$title\007""
 		CMD_PREFIX="\"\""
 		;;
 esac 
-
-
-
-function location() {
-	case "$PROXY_LOC" in 
-		"local")
-			if [[ $DOCKER_CONFIG == *"fileauth"* ]]; then 
-				PROXY_LOC="local fileauth"
-				proxy_location "local fileauth" $RESULTS_PATH
-			elif [[ $DOCKER_CONFIG == *"ccsapi"* ]]; then 
-				PROXY_LOC="local ccsapi"
-				proxy_location "local ccsapi" $RESULTS_PATH
-			else 
-				helpme
-				exit 1
-			fi 
-			;;
-		"dev-mon01")
-			PROXY_LOC="dev-mon01"
-			proxy_location "dev-mon01" $RESULTS_PATH
-			;;
-	esac 
-
-}
 
 
 # Docker ps
@@ -183,8 +116,7 @@ function test_ps() {
 	local output
 	local timestamp=$(date +"%Y%m%d.%H%M%S")
 
-	# local STARTTIME=$(date +%s)
-	local STARTTIME=$(date +%s) # milliseconds
+	local STARTTIME=$(date +%s) 
 	output=$(generic_docker_command $CMD_PREFIX "ps" 2>&1)
 	local RESULT=$?
 	local ENDTIME=$(date +%s)
@@ -195,7 +127,6 @@ function test_ps() {
 	local IS_SUCCESS=$(check_result $RESULT $(($ENDTIME - $STARTTIME)) $TEST_COUNT "Docker ps" $RESULTS_PATH $EXPECTED "$output" "$timestamp" "$TENANT_ID" "$TEST_TYPE")
 	SUCCESS_COUNT=$(($SUCCESS_COUNT + $IS_SUCCESS))
 
-	# So test count increment, success count increment, write result to file. 
 	echo ""
 }
 
@@ -239,8 +170,6 @@ function test_create_nonet() {
 
 	echo "$output"
 	
-
-	# local IS_SUCCESS=$(check_result $RESULT $(($ENDTIME - $STARTTIME)) $TEST_COUNT "Docker run; no net; name = $NAME" $RESULTS_PATH $EXPECTED "$output" "$timestamp" "$TENANT_ID" "$TEST_TYPE")
 	local IS_SUCCESS=$(check_result $RESULT $(($ENDTIME - $STARTTIME)) $TEST_COUNT "Docker run $NAME w/o net" $RESULTS_PATH $EXPECTED "$output" "$timestamp" "$TENANT_ID" "$TEST_TYPE")
 	SUCCESS_COUNT=$(($SUCCESS_COUNT + $IS_SUCCESS))
 	echo ""
@@ -264,7 +193,6 @@ function test_create_net() {
 	echo "$output"
 	
 
-	# local IS_SUCCESS=$(check_result $RESULT $(($ENDTIME - $STARTTIME)) $TEST_COUNT "Docker run; w/net; name = $NAME" $RESULTS_PATH $EXPECTED "$output" "$timestamp" "$TENANT_ID" "$TEST_TYPE")
 	local IS_SUCCESS=$(check_result $RESULT $(($ENDTIME - $STARTTIME)) $TEST_COUNT "Docker run $NAME w/net" $RESULTS_PATH $EXPECTED "$output" "$timestamp" "$TENANT_ID" "$TEST_TYPE")
 	SUCCESS_COUNT=$(($SUCCESS_COUNT + $IS_SUCCESS))
 	echo ""
@@ -288,7 +216,6 @@ function test_inspect() {
 
 	echo "$output"
 	
-	# local IS_SUCCESS=$(check_result $RESULT $(($ENDTIME - $STARTTIME)) $TEST_COUNT "Docker inspect; name = $NAME" $RESULTS_PATH $EXPECTED "$output" "$timestamp" "$TENANT_ID" "$TEST_TYPE")
 	local IS_SUCCESS=$(check_result $RESULT $(($ENDTIME - $STARTTIME)) $TEST_COUNT "Docker inspect $NAME" $RESULTS_PATH $EXPECTED "$output" "$timestamp" "$TENANT_ID" "$TEST_TYPE")
 	SUCCESS_COUNT=$(($SUCCESS_COUNT + $IS_SUCCESS))
 	echo ""
@@ -312,7 +239,6 @@ function test_stop() {
 	echo "$output"
 	
 
-	# local IS_SUCCESS=$(check_result $RESULT $(($ENDTIME - $STARTTIME)) $TEST_COUNT "Docker stop; name = $NAME" $RESULTS_PATH $EXPECTED "$output" "$timestamp" "$TENANT_ID" "$TEST_TYPE")
 	local IS_SUCCESS=$(check_result $RESULT $(($ENDTIME - $STARTTIME)) $TEST_COUNT "Docker stop $NAME" $RESULTS_PATH $EXPECTED "$output" "$timestamp" "$TENANT_ID" "$TEST_TYPE")
 	SUCCESS_COUNT=$(($SUCCESS_COUNT + $IS_SUCCESS))
 	echo ""
@@ -337,7 +263,6 @@ function test_start() {
 
 	echo "$output"
 
-	# local IS_SUCCESS=$(check_result $RESULT $(($ENDTIME - $STARTTIME)) $TEST_COUNT "Docker start; name = $NAME" $RESULTS_PATH $EXPECTED "$output" "$timestamp" "$TENANT_ID" "$TEST_TYPE")
 	local IS_SUCCESS=$(check_result $RESULT $(($ENDTIME - $STARTTIME)) $TEST_COUNT "Docker start $NAME" $RESULTS_PATH $EXPECTED "$output" "$timestamp" "$TENANT_ID" "$TEST_TYPE")
 	SUCCESS_COUNT=$(($SUCCESS_COUNT + $IS_SUCCESS))
 	echo ""
@@ -362,7 +287,6 @@ function test_rm() {
 	
 	echo "$output"
 
-	# local IS_SUCCESS=$(check_result $RESULT $(($ENDTIME - $STARTTIME)) $TEST_COUNT "Docker rm; name = $NAME" $RESULTS_PATH $EXPECTED "$output" "$timestamp" "$TENANT_ID" "$TEST_TYPE")
 	local IS_SUCCESS=$(check_result $RESULT $(($ENDTIME - $STARTTIME)) $TEST_COUNT "Docker rm $NAME" $RESULTS_PATH $EXPECTED "$output" "$timestamp" "$TENANT_ID" "$TEST_TYPE")
 	SUCCESS_COUNT=$(($SUCCESS_COUNT + $IS_SUCCESS))
 	echo ""
@@ -385,7 +309,6 @@ function test_rm_f() {
 	
 	echo "$output"
 
-	# local IS_SUCCESS=$(check_result $RESULT $(($ENDTIME - $STARTTIME)) $TEST_COUNT "Docker rm -f; name = $NAME" $RESULTS_PATH $EXPECTED "$output" "$timestamp" "$TENANT_ID" "$TEST_TYPE")
 	local IS_SUCCESS=$(check_result $RESULT $(($ENDTIME - $STARTTIME)) $TEST_COUNT "Docker rm -f $NAME" $RESULTS_PATH $EXPECTED "$output" "$timestamp" "$TENANT_ID" "$TEST_TYPE")
 	SUCCESS_COUNT=$(($SUCCESS_COUNT + $IS_SUCCESS))
 	echo ""
@@ -430,7 +353,6 @@ function test_network_inspect() {
 	
 	echo "$output"
 
-	# local IS_SUCCESS=$(check_result $RESULT $(($ENDTIME - $STARTTIME)) $TEST_COUNT "Docker network inspect; name = $NAME" $RESULTS_PATH $EXPECTED "$output" "$timestamp" "$TENANT_ID" "$TEST_TYPE")
 	local IS_SUCCESS=$(check_result $RESULT $(($ENDTIME - $STARTTIME)) $TEST_COUNT "Docker network inspect $NAME" $RESULTS_PATH $EXPECTED "$output" "$timestamp" "$TENANT_ID" "$TEST_TYPE")
 	SUCCESS_COUNT=$(($SUCCESS_COUNT + $IS_SUCCESS))
 	echo ""
@@ -444,38 +366,27 @@ function main() {
 	fi
 	touch $RESULTS_PATH
 
-
-	#location 
-
-	# Try the no net ones first. 
+	 
 	NONET_COUNTER=1
 	while [  $NONET_COUNTER -le $NUM_NO_NET_CONTAINERS ]; do
-		test_ps 0 # Test 1
+		test_ps 0 
 
-		test_inspect "$TENANT_ID""_test1" 1 # Test 2
+		test_inspect "$TENANT_ID""_test1" 1 
 
-		test_create_nonet "$TENANT_ID""_nonet_test""$NONET_COUNTER" 0 # Test 3
-		test_ps 0 # Test 4
+		test_create_nonet "$TENANT_ID""_nonet_test""$NONET_COUNTER" 0 
+		test_ps 0 
 
-		# Test 5
-		# Test 6
+		test_inspect "$TENANT_ID""_nonet_test""$NONET_COUNTER" 0 
 
-		test_inspect "$TENANT_ID""_nonet_test""$NONET_COUNTER" 0 # Test 7 
-
-		# Docker stop
 		test_stop "$TENANT_ID""_test1" 1 # Should fail because doesn't exist
-		test_stop "$TENANT_ID""_nonet_test""$NONET_COUNTER" 0 # Test 8 
-		test_ps_a 0 # Test 9
-		test_ps 0 # Test 10
+		test_stop "$TENANT_ID""_nonet_test""$NONET_COUNTER" 0 
+		test_ps_a 0 
+		test_ps 0 
 
-		# Test start 
+		test_start "$TENANT_ID""_test1" 1 # Doesn't exist
+		test_start "$TENANT_ID""_nonet_test""$NONET_COUNTER" 0 
 
-		# Start - if resource dne, should error, if already up, nothing, and should start it up if stopped. 
-		#echo "Docker start nonet_test1 should fail" >> $RESULTS_PATH
-		test_start "$TENANT_ID""_test1" 1 # Fails because doesn't exist
-		test_start "$TENANT_ID""_nonet_test""$NONET_COUNTER" 0 # Test 14
-
-		test_ps_a 0 # Test 16
+		test_ps_a 0 
 
 
 		# Test can't remove without stopping first
@@ -485,32 +396,22 @@ function main() {
 	done 
 
 
-	# Now the net ones. 
 	NET_COUNTER=1
 	while [  $NET_COUNTER -le $NUM_NET_CONTAINERS ]; do
-		test_ps 0 # Test 1
+		test_ps 0 
 
-		test_create_net "$TENANT_ID""_net_test""$NET_COUNTER" 0 # Test 3
-		test_ps 0 # Test 4
+		test_create_net "$TENANT_ID""_net_test""$NET_COUNTER" 0 
+		test_ps 0 
 
-		# Test 5
-		# Test 6
+		test_inspect "$TENANT_ID""_net_test""$NET_COUNTER" 0 
 
-		test_inspect "$TENANT_ID""_net_test""$NET_COUNTER" 0 # Test 7 
+		test_stop "$TENANT_ID""_net_test""$NET_COUNTER" 0 
+		test_ps_a 0 
+		test_ps 0 
 
-		# Docker stop
-		test_stop "$TENANT_ID""_net_test""$NET_COUNTER" 0 # Test 8 
-		test_ps_a 0 # Test 9
-		test_ps 0 # Test 10
+		test_start "$TENANT_ID""_net_test""$NET_COUNTER" 0 
 
-		# Test start 
-
-		# Start - if resource dne, should error, if already up, nothing, and should start it up if stopped. 
-		#echo "Docker start nonet_test1 should fail" >> $RESULTS_PATH
-		test_start "$TENANT_ID""_net_test""$NET_COUNTER" 0 # Test 14
-
-		test_ps_a 0 # Test 16
-
+		test_ps_a 0 
 
 		# Test can't remove without stopping first
 		test_rm "$TENANT_ID""_net_test""$NET_COUNTER" 1 
@@ -519,13 +420,6 @@ function main() {
 	done 
 
 
-
-	# REMOVE THEM ALL AT THE ENNNDDD
-	# Test 11
-	# Test 12
-	# Test 13
-
-	# Trying removing some with rm -f too 
 	NONET_COUNTER=1
 	while [  $NONET_COUNTER -le $NUM_NO_NET_CONTAINERS ]; do
 		if [ $(( $NONET_COUNTER % 2 )) -eq 0 ]; then 
@@ -539,31 +433,12 @@ function main() {
 	done 
 
 
-	# NET_COUNTER=1
-	# while [  $NET_COUNTER -le $NUM_NET_CONTAINERS ]; do
-	# 	test_rm_f "$TENANT_ID""_net_test""$NET_COUNTER" 0 
-
-	# 	let NET_COUNTER=NET_COUNTER+1
-	# done 
-
-
-
 	test_network_ls 0
-
-	# REMOVING NETWORK INSPECT FOR NOW 
-
-	# test_network_inspect $try_net_id 0
-
-	# test_network_inspect "default" 0 
-
 
 	# Make sure everything is clean at the end 
 	test_ps_a 0
 	test_ps 0 
 
-
-
-	# SUM THE RESULTS
 	sum_results "Docker Containers" $RESULTS_PATH $TEST_COUNT $SUCCESS_COUNT $starttime
 
 }
