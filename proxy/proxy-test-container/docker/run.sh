53fe4b8e9126a1fcc6d41d8a3fc6eec5472e39be
Tommaso Madonia <tmadonia@us.ibm.com>
2016-08-16 13:11:40 -0400

    Automatically run tests for all tenants when running on CI

--- proxy/proxy-test-container/docker/run.sh
+++ proxy/proxy-test-container/docker/run.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-docker run -v ~/.openradiant/envs:/tests/certs:ro -v api-proxy-tests-logs:/tests/logs --net="host" api-proxy-tests "$@"
+tenants=()
+if [[ `echo "$CI" | tr '[:upper:]' '[:lower:]'` == "true" ]]; then
+    IFS=$'\n' tenants=(`docker exec api-proxy cat /api-proxy/creds.json \
+        | tail -n +2 \
+        | grep -oP "((?<=\"TLS_path\":\")|(?<=\"Space_id\":\"))[^\"]+" \
+        | rev \
+        | sed 'N;s/\(.*\)\n\([^/]*\)\(\/\)\([^/]*\)\(.*\)/t-\n\2\:\1\:\4/' \
+        | rev`)
+fi
+
+docker run -v ~/.openradiant/envs:/tests/certs:ro -v api-proxy-tests-logs:/tests/logs --net="host" api-proxy-tests "${tenants[@]}" "$@"
