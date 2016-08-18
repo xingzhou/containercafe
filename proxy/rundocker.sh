250c91e3e71a073ce1176623830bc34f85a84ca8
Tommaso Madonia <tmadonia@us.ibm.com>
2016-08-18 16:41:07 -0400

    Proxy scripts cleanup
    - Replaced tabs in favor of 4 spaces
    - Quoted variables
    - Removed useless comments

--- proxy/rundocker.sh
+++ proxy/rundocker.sh
@@ -4,63 +4,58 @@
 # Help menu
 helpme()
 {
-	cat <<HELPMEHELPME
+    cat <<HELPMEHELPME
 
 Syntax: ${0} <env_name> <-d>
 Where:
-	env_name - name of the environment, e.g: dev-vbox
-	-d - run api-proxy container in the background (optional)
-	
+    env_name - name of the environment, e.g: dev-vbox
+    -d - run api-proxy container in the background (optional)
+
 HELPMEHELPME
 }
 
 if [[ "$1" == "" || "$1" == "-d" ]] ; then
-	echo 'env_name must be set'
-	helpme
-	exit 1
+    echo 'env_name must be set'
+    helpme
+    exit 1
 fi
-env_name=$1
+env_name="$1"
 
 # manage certificates - copy them to admin-certs
-CERT_MASTER=../ansible/certs/dev-vbox-radiant01
-CERTS=~/.openradiant/envs/$env_name
-ACERTS=$CERTS/admin-certs
+CERT_MASTER="../ansible/certs/dev-vbox-radiant01"
+CERTS="$HOME/.openradiant/envs/$env_name"
+ACERTS="$CERTS/admin-certs"
 if [ ! -d "$CERT_MASTER" ]; then
   echo "missing $CERT_MASTER directory. Execute ansible scripts first"
   exit 99
 fi
 
 if [ ! -d "$ACERTS" ]; then
-	mkdir -p $ACERTS
-	
-	cp -f $CERT_MASTER/ca* $ACERTS
-	cp -f $CERT_MASTER/admin-key.pem $ACERTS/kadmin.key
-	cp -f $CERT_MASTER/admin.pem $ACERTS/kadmin.pem
-	
-	cp -f ../ansible/roles/keygen/files/api-proxy-openssl.cnf $ACERTS/api-proxy-openssl.cnf
-	
-	# generate all proxy certs
-	./gen_server_certs.sh $ACERTS
+    mkdir -p "$ACERTS"
+    
+    cp -f "$CERT_MASTER/ca"* "$ACERTS"
+    cp -f "$CERT_MASTER/admin-key.pem" "$ACERTS/kadmin.key"
+    cp -f "$CERT_MASTER/admin.pem" "$ACERTS/kadmin.pem"
+    
+    cp -f ../ansible/roles/keygen/files/api-proxy-openssl.cnf "$ACERTS/api-proxy-openssl.cnf"
+    
+    # generate all proxy certs
+    ./gen_server_certs.sh "$ACERTS"
 else
-	echo "WARNING: using the existing certs in $ACERTS"
-	echo "To recreate the certs, delete this directory"
-fi	
-
-#HOSTS=$(cat hosts)
-#echo $HOSTS
-# remove the previous container instance 
-CMD="docker rm -f api-proxy"
-echo $CMD
-$(echo $CMD)
+    echo "WARNING: using the existing certs in $ACERTS"
+    echo "To recreate the certs, delete this directory"
+fi    
 
 # to run container as a daemon use `-d` flag:
-EXTRA_FLAGS=""
+EXTRA_FLAGS=()
 if [ "$2" == "-d" ] ; then
-	EXTRA_FLAGS="-d"
+	EXTRA_FLAGS+=("-d")
 fi
 
-# start new container instance. Map the volume to CERTS
-CMD="docker run $EXTRA_FLAGS -v $CERTS:/opt/tls_certs -p 8087:8087 -e env_name=$env_name --name api-proxy api-proxy"
+set -x
 
-echo $CMD
-$(echo $CMD)
+# remove the previous container instance 
+docker rm -f api-proxy
+
+# start new container instance. Map the volume to CERTS
+docker run "${EXTRA_FLAGS[@]}" -v "$CERTS":/opt/tls_certs -p 8087:8087 -e "env_name=$env_name" --name api-proxy api-proxy
--- proxy/start_proxy.sh
+++ proxy/start_proxy.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
+
 # override TLS_OUTBOUND to support K8s
 export tls_outbound=true
 export tls_inbound=true
-export server_key_file=admin-certs/hjserver.key 
-export server_cert_file=admin-certs/hjserver.pem
-export ca_cert_file=admin-certs/ca.pem
+export server_key_file="admin-certs/hjserver.key"
+export server_cert_file="admin-certs/hjserver.pem"
+export ca_cert_file="admin-certs/ca.pem"
 export stub_auth_file="creds.json"
 export kube_admin_key_file="admin-certs/kadmin.key"
 export kube_admin_cert_file="admin-certs/kadmin.pem"
@@ -11,12 +13,12 @@ export kube_admin_cert_file="admin-certs/kadmin.pem"
 # dev-mon01:
 export ccsapi_host=10.140.34.174:8081
 export consul_ip=10.140.34.174
-# radiant
-# start proxy
 
-cp -f ../ansible/certs/dev-vbox-radiant01/ca* admin-certs
-cp -f ../ansible/certs/dev-vbox-radiant01/admin-key.pem admin-certs/kadmin.key
-cp -f ../ansible/certs/dev-vbox-radiant01/admin.pem admin-certs/kadmin.pem
+# copy the certs
+cp -f ../ansible/certs/dev-vbox-radiant01/ca* "$(dirname "$ca_cert_file")"
+cp -f ../ansible/certs/dev-vbox-radiant01/admin-key.pem "$kube_admin_key_file"
+cp -f ../ansible/certs/dev-vbox-radiant01/admin.pem "$kube_admin_cert_file"
 ./gen_server_certs.sh
 
+# start proxy
 bin/hijack 
