5f5e2bc38d44ac5dfabd57de8dc92d58049f24ec
Mariusz Sabath <sabath@us.ibm.com>
2016-09-06 13:40:50 -0400

    Added code to process the API key provided in the request header

--- proxy/src/auth/fileauth.go
+++ proxy/src/auth/fileauth.go
@@ -16,31 +16,44 @@ func FileAuth(r *http.Request) (creds Creds) {
 	
 	// let's start with 400 Bad Request 
 	creds.Status = 400
-	if (r.TLS == nil || len(r.TLS.PeerCertificates) < 1) {
-		glog.Error("Request missing client TLS certificate")
-		return
-	}
-
-	glog.Infof("Total of TLS certificate(s) found in request: %v", len(r.TLS.PeerCertificates))
-	cn := ""
-
-	for _, cert := range r.TLS.PeerCertificates {
-		glog.Infof("CN from the client cert: %v", cert.Subject.CommonName)
-		cn = cert.Subject.CommonName
-		
-		// cert could be CA:
-		if (len(cn) != 0 && cn != "containers-api-dev.stage1.ng.bluemix.net") {
-			break
+	Apikey := ""
+	
+	if conf.IsApiKeyHeaderEnabled() {
+	// configuration specifies that key is passed in the header
+		// Apikey = r.Header.Get("X-Tls-Client-Dn")
+		Apikey = r.Header.Get(conf.GetApiKeyHeader())
+		if Apikey == "" {
+			glog.Error("enable_api_key_header=true, but header is missing")
+			return
 		}
 	}
-	if cn == "" {
-		return
-	}
 	
-	Apikey := cn
-	creds.Status = 404
-	// Apikey := r.Header.Get("X-Tls-Client-Dn")
-
+	if conf.IsApiKeyCertEnabled() {
+	// configuration specifies that key is passed through the certificate
+		if (r.TLS == nil || len(r.TLS.PeerCertificates) < 1) {
+			glog.Error("Request missing client TLS certificate")
+			return
+		}
+		glog.Infof("Total of TLS certificate(s) found in request: %v", len(r.TLS.PeerCertificates))
+		cn := ""
+	
+		for _, cert := range r.TLS.PeerCertificates {
+			glog.Infof("CN from the client cert: %v", cert.Subject.CommonName)
+			cn = cert.Subject.CommonName
+			
+			// cert could be CA:
+			if (len(cn) != 0 && cn != "containers-api-dev.stage1.ng.bluemix.net") {
+				break
+			}
+		}
+		if cn == "" {
+			return
+		}
+		
+		Apikey = cn
+		creds.Status = 404
+		// Apikey := r.Header.Get("X-Tls-Client-Dn")
+	}
 	fp, err := os.Open(conf.GetStubAuthFile())
 	if err != nil{
 		glog.Error(err)
