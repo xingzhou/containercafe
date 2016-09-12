389bc39a7510d1e917e56ee7beb2aa591d593ded
Tommaso Madonia <tmadonia@us.ibm.com>
2016-09-12 13:00:55 -0400

    Removed dockerize folder
    - Deleted dockerize folder
    - Updated ansible proxy role
    - Updated proxy image build script
    - Updated proxy documentation

--- proxy/builddocker.sh
+++ proxy/builddocker.sh
@@ -6,15 +6,8 @@ function build_proxy_image {
     local context="$2"
     shift 2
     
-    # copy src into Dockerfile directory
-    [[ -d "$context/src" ]] && rm -rf "$context/src"
-    cp -r src "$context"
-
-    # copy scripts into Dockerfile directory
-    cp create_tenant.sh mk_user_cert.sh mk_kubeconfig.sh "$context"
-    
     # build the docker image
     docker build "$@" -t "$name" "$context"
 }
 
-build_proxy_image "api-proxy" "dockerize/" "$@"
+build_proxy_image "api-proxy" "." "$@"
